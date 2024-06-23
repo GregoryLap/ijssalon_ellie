fad61b99ddaf993b8bbffc9aeadf163d8e1df96a
Gregory <Pauwelynjoke@gmail.com>
2024-06-23 09:23:26 +0200

    reclameboodschap toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -4,3 +4,5 @@ prijzen = {
     "chocolade":5
 }
 aanbieding = prijzen["vannille"] * 0.8
+reclame_tekst ="Vandaag in de aanbieding : vanille-ijs, 1 liter - slechts €"+str(aanbieding) 
+print(reclame_tekst)
